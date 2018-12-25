943bbd1f58733efc571d70654cf4411a80c3b12b
KARTHIK CHANDRA CHERUKURI <CHERUKURI.KC@GMAIL.COM>
2018-12-25 00:50:05 -0500

    961 - Solution

--- src/easy/_961.rs
+++ src/easy/_961.rs
@@ -2,8 +2,15 @@ struct Solution;
 
 impl Solution {
     pub fn repeated_n_times(a: Vec<i32>) -> i32 {
-
-        0
+        let mut count_arr = [0; 10000];
+        let n = a.len() / 2;
+        for i in a {
+            count_arr[i as usize] += 1;
+            if count_arr[i as usize] == n {
+                return i;
+            }
+        }
+        -1
     }
 }
 
@@ -13,6 +20,6 @@ mod test {
 
     #[test]
     fn test_repeated_n_times() {
-
+        assert_eq!(Solution::repeated_n_times(vec![2, 4, 7, 2, 7, 2]), 2);
     }
 }
